17b0e90f845cad910f9ad4b0302a9e71990fcf65
lsminter <lsminteris@gmail.com>
2017-10-24 08:52:15 -0700

    prepare plunker

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//main entry point
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppModule} from './app';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
