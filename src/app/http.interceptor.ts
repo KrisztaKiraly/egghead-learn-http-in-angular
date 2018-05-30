f7759e8114ed39209dd12e4572a41ac36c2af60f
lsminter <lsminteris@gmail.com>
2018-05-30 16:54:01 -0700

    added files to run application locally and updated dependencies

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpResponse,
-  HttpErrorResponse,
-  HttpHandler,
-  HttpEvent
-} from '@angular/common/http';
 
+import { HttpInterceptor, HttpRequest, HttpErrorResponse, HttpResponse, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
-export class MyHttpLogInterceptor implements HttpInterceptor {
+export class MyLogHttpInterceptor implements HttpInterceptor {
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('processing request', request);
 
@@ -28,13 +22,16 @@ export class MyHttpLogInterceptor implements HttpInterceptor {
         if (ev instanceof HttpResponse) {
           console.log('processing response', ev);
         }
+
+        return ev;
       })
       .catch(response => {
         if (response instanceof HttpErrorResponse) {
-          console.log('Processing http error', response);
+          console.log('processing error response', response);
         }
 
         return Observable.throw(response);
       });
   }
+
 }
