0ae009993990ed7ae1a698fa783a6255fa031c74
lsminter <lsminteris@gmail.com>
2018-05-30 16:19:04 -0700

    added files to run application locally and updated dependencies

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import 'rxjs/add/operator/retry'
-import 'rxjs/add/operator/retryWhen'
-import 'rxjs/add/operator/mergeMap'
-import 'rxjs/add/operator/delay'
-import 'rxjs/add/observable/throw'
-import 'rxjs/add/observable/of'
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class PeopleService {
@@ -16,20 +17,20 @@ export class PeopleService {
 
   fetchPeople(): Observable<Object> {
     return this.http
-     // .get('data/people.json');
-      .get('data/unavailable.json')
+      // .get('/assets/data/people.json');
+      .get('/assets/data/unavailable.json')
       .retryWhen(err => {
         let retries = 3;
-        
+
         return err
           .delay(1000)
           .flatMap((err) => {
             if (retries-- > 0) {
               return Observable.of(err);
             } else {
-              return Observable.throw(err)
+              return Observable.throw(err);
             }
-          })
+          });
       });
   }
 
